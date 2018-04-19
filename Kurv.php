e57524ad9804e7e2f0a1ded0447cefdb287b32c1
Mathias Bredahl <mathias1406@hotmail.dk>
2018-04-19 11:04:45 +0200

    Videre
    Hej

--- Kurv.php
+++ Kurv.php
@@ -1,8 +1,10 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Regnebueslik</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="Beskrivelse af side" />
+</head>
+<body>
+</body>
+</html>
