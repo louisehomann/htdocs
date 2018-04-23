8527170fd31009e82288ab982cb9aebba95fcbdb
Jesper Nansen <jesper_nansen@hotmail.com>
2018-04-23 10:33:13 +0200

    vv

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
   <meta name="description" content="Beskrivelse af side" />
 </head>
 <body>
-    
+     <header>
+    <h1>Most important heading here</h1>
+    <h3>Less important heading here</h3>
+    <p>Some additional information here</p>
+  </header>
 </body>
 </html>
