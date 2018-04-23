7abeea58fdc8fa0141c5f18fec47011d500205c8
Louise <louisehomann@gmail.com>
2018-04-23 11:41:28 +0200

    --

--- index.php
+++ index.php
@@ -4,16 +4,73 @@
   <title>Regnebueslik</title>
   <meta charset="utf-8" />
   <meta name="description" content="Beskrivelse af side" />
+  
+  <style> 
+      body {
+          margin: 0;
+          font-family: Arial, Helvetica, sans-serif;
+      }
+      
+      .topnav {
+          overflow: hidden;
+          background-color: #333;
+          
+      }
+      
+     /* Tekst på navbar*/
+      .topnav a {
+          float: left;
+          color: #f2f2f2;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+          font-size: 17px;
+      }
+      
+      /*skygge på navbar*/
+      .topnav a:hover {
+          background-color: #ddd;
+          color: black;
+      }
+      
+      /*farver på navbar*/
+      .forside {
+          background-color: #ff3333;
+          color: white;
+      }
+      
+      .vingummi {
+          background-color: #33dd33;
+          color: #000000;
+      
+      }
+      
+      .lakrids {
+          background-color: #33dd33;
+          color: #000000;
+      }
+      
+      .chokolade {
+          background-color: #33dd33;
+          color: #000000;
+      }
+    
+   </style>   
+   
 </head>
 <body>
+    
      <header>
-    <h1>Regnbueslik</h1>
-  
+   <h1>Regnbueslik</h1>
+      
     <div class="topnav">
-  <a class="active" href="#home">Forside</a>
-  <a href="#news">Vingummi</a>
-  <a href="#contact">Lakrids</a>
-  <a href="#about">Chockolade</a>
+  <a class="forside" href="#forside">Forside</a>
+  <a class="vingummi" href="#vingummi">Vingummi</a>
+  <a class="lakrids" href="#lakrids">Lakrids</a>
+  <a class= "chokolade" href="#chokolade">Chockolade</a>
+ 
+ 
+  
 </div>
 
   </header>
