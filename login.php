f5a24e5bf0125738f4b66257990141fb02b30483
Mathias Bredahl <mathias1406@hotmail.dk>
2018-04-25 15:15:07 +0200

    hej
    du

--- login.php
+++ login.php
@@ -4,7 +4,16 @@
         <title></title>
     </head>
     <body>
-        
+        <div>
+            <p> Login </p>
+            <form method="post" action="#"> 
+                <label for="username">Username:</label>
+                <input type="text" name="brugernavn">
+                <label for="password">Password:</label>
+                <input type="text" name="password">
+                
+            </form>
+        </div>
     
       
     </body>
