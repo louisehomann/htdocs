db32a59a6a979d6e36cff1a95043d72c7da65fe1
Mathias Bredahl <mathias1406@hotmail.dk>
2018-04-25 15:05:21 +0200

    hej
    du

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
         
         
         <div>
-            <a> Login| </a>
+            <a class="login" href="#login.php"> Login| </a>
             <a> Min bruger| </a>
             <a> Min pose </a>
             
